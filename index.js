98f30b56d01172199d64a8eae9b145c6a28c443a
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-02-18 11:23:03 +0530

    add morgan for logging and update package.json and package-lock.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 
 import dotenv from "dotenv";
 dotenv.config({
@@ -7,6 +8,31 @@ dotenv.config({
 
 const app = express();
 const PORT = process.env.PORT;
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan("div"));
+}
+
+
+//Global Error Handling
+app.use((err, req, res, next)=>{
+    console.err(err.stack)
+    res.status(err.status || 500).json(
+        {
+            status: "error",
+            message: err.message || "Internal Server error", ...PORT(process.env.NODE_ENV === 'development' && {stack: err.stack})
+        }
+    )
+})
+
+
+
+
+app.use(express.json({limit: "10kb"}));
+app.use(express.urlencoded())
+
+
+
 
 app.listen(PORT, ()=>{
     console.log("Server is runiing at PORT: " +PORT);
