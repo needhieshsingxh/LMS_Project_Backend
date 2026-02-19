5537d09e070f3742715da516a7ba2b960f37da15
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-02-19 11:55:04 +0530

    add course progress model with schema definition, completion logic, and last accessed timestamp

---
+++ src/Models/courseProgress.model.js
@@ -0,0 +1,77 @@
+import mongoose, {Schema} from "mongoose";
+
+const lectureProgressSchema = new mongoose.Schema({
+  lecture: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lecture",
+    required: [true, "Lecture reference is required"]
+  },
+  isCompleted:{
+    type: Boolean,
+    default: false
+  },
+  watchTime: {
+    type: Number,
+    default: 0
+  }, 
+  lastWatched:{
+    type: Date,
+    default: Date.now
+
+  }
+})
+
+const courseProgressSchema = new mongoose.Schema({
+    use:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: [true, "User reference is required"]
+    },
+    course:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: [true, "Course reference is required"]
+    },
+    isCompleted:{
+        type: Boolean, 
+        default: false
+    },
+    completionPercentage: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 100
+    },
+
+    lectureProgress: [lectureProgressSchema],
+    lastAccessed: {
+        type: Date,
+        default: Date.now,
+    },
+    
+
+
+},{
+ timestamps: true,
+toJSON: {virtuals: true},
+toObject: {virtuals: true}
+})
+
+
+
+courseProgressSchema.pre('save', function(){
+    if(this.lectureProgress.length > 0){
+        const completedLectures = this.lectureProgress.filter(lp => lp.isCompleted).length
+        const completionPercentage =  Math.round((completedLectures / this.lectureProgress.length) * 100)
+        this.isCompleted = this.completionPercentage === 100
+    }
+    next()
+})
+
+courseProgressSchema.methods.updateLastAccessed = function(){
+    this.lastAccessed = Dete.now()
+    return this.save({ValidateBeforeSave: false})
+}
+
+
+export const courseProgress = mongoose.model('courseProgress', courseProgressSchema);
